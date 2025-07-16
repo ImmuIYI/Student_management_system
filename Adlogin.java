88807e45c490e1fd38dbe32d2d36771793c2c7b2
Imran shaik <113525638+ImmuIYI@users.noreply.github.com>
2025-07-16 11:10:40 +0530

    Update Adlogin.java

--- Adlogin.java
+++ Adlogin.java
@@ -140,7 +140,7 @@ public class Adlogin extends javax.swing.JFrame {
         }
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/regdetails","root","Immu@2602");
+            Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/regdetails","******","*********");
                 
             PreparedStatement pstm = con.prepareStatement("SELECT * FROM Adid WHERE adminid = ? AND apass = ? ");
             pstm.setString(1,user);
